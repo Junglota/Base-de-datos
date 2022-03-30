7f33ac60cf19e9be4aaad19bca91b1e4ca62d2da
Junglota <20197415@itla.edu.do>
2022-03-29 22:55:43 -0400

    Primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Prueba_2_login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.User = new System.Windows.Forms.Label();
+            this.Contra = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // User
+            // 
+            this.User.AutoSize = true;
+            this.User.Location = new System.Drawing.Point(68, 86);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(43, 13);
+            this.User.TabIndex = 0;
+            this.User.Text = "Usuario";
+            this.User.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Contra
+            // 
+            this.Contra.AutoSize = true;
+            this.Contra.Location = new System.Drawing.Point(68, 156);
+            this.Contra.Name = "Contra";
+            this.Contra.Size = new System.Drawing.Size(61, 13);
+            this.Contra.TabIndex = 1;
+            this.Contra.Text = "Contraseña";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(97, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(185, 238);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(99, 33);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Log_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 365);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Contra);
+            this.Controls.Add(this.User);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label User;
+        private System.Windows.Forms.Label Contra;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Login;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using Prueba_2_login.mysql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_2_login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Log_Click(object sender, EventArgs e)
+        {
+            conexion.obtenerConexion();
+            MessageBox.Show("Conectado");
+        }
+    }
+}
